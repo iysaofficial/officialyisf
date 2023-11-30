b29a156ee229214f69d7c4c057945ff4cfbf885c
unknown <youngscientist.iysa@gmail.com>
2023-11-30 09:45:18 +0700

    tambah logo udayan

--- src/components/Supported.jsx
+++ src/components/Supported.jsx
@@ -1,9 +1,12 @@
 import "./css/supported.css";
 
 export default function Supported() {
-    const dataLogo = [
-        "images/logo/iysa.png",
+    const dataLogo1 = [
+        "images/logo/iysa.png"
+    ]
+    const dataLogo2 = [
         "images/logo/undip.png",
+        "images/logo/Udayana.png",
         "images/logo/buca.png",
     ]
     return (
@@ -14,7 +17,16 @@ export default function Supported() {
                         <div className="row">
 
                             {
-                                dataLogo.map(function(ImgLogo) {
+                                dataLogo1.map(function(ImgLogo) {
+                                    return(
+                                        <div className="logo-iysa col-lg-4 col-md-4 mt-5 mt-md-0 text-center">
+                                            <img className=" col-md-8" src={ImgLogo} />
+                                        </div>
+                                    )
+                                })
+                            }
+                            { 
+                                dataLogo2.map(function(ImgLogo) {
                                     return(
                                         <div className="Logo-Support col-lg-4 col-md-4 mt-5 mt-md-0 text-center">
                                             <img className=" col-md-8" src={ImgLogo} />
