57e7ebcbcb6190019ccd9a2bafb6e9e9eb5c7530
unknown <youngscientist.iysa@gmail.com>
2023-12-05 12:05:19 +0700

    add supported by

--- src/components/Supported.jsx
+++ src/components/Supported.jsx
@@ -1,40 +1,41 @@
 import "./css/supported.css";
 
 export default function Supported() {
-    const dataLogo1 = [
-        "images/logo/iysa.png"
-    ]
-    const dataLogo2 = [
-        "images/logo/undip.png",
-        // "images/logo/Udayana.png",
-        "images/logo/buca.png",
-    ]
+
     return (
         <>
-            <section className="section pt-0 position-relative pull-top">
+            <section className="section-Organizer pt-0 position-relative pull-top">
                 <div className="container">
-                    <div className="rounded shadow p-5 bg-white">
-                        <div className="row">
-
-                            {
-                                dataLogo1.map(function(ImgLogo) {
-                                    return(
-                                        <div className="logo-iysa col-lg-4 col-md-4 mt-5 mt-md-0 text-center">
-                                            <img className=" col-md-8" src={ImgLogo} />
-                                        </div>
-                                    )
-                                })
-                            }
-                            { 
-                                dataLogo2.map(function(ImgLogo) {
-                                    return(
-                                        <div className="Logo-Support col-lg-4 col-md-4 mt-5 mt-md-0 text-center">
-                                            <img className=" col-md-8" src={ImgLogo} />
-                                        </div>
-                                    )
-                                }) 
-                            }
-
+                        <h1 className="fw-bold text-center">Organized By</h1>
+                    <div className="box p-3 bg-white">
+                        <div className="row text-center mx-auto">
+                            <div className="logo-iysa col-lg-4 col-md-4 mt-5 mt-lg-0">
+                                <img className="" src="images/logo/iysa.png" />
+                            </div>
+                            <div className="logo-teknikundip col-lg-4 mt-5 mt-lg-0 text-center">
+                                <img className="" src="images/logo/TeknikUNDIP.WebP" />
+                            </div>
+                            <div className="Logo-Support col-lg-4 col-md-4 mt-5 mt-lg-0">
+                                <img className="" src="images/logo/buca.png" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="section-Supported pt-0 position-relative pull-top">
+                <div className="container">
+                        <h1 className="fw-bold text-center">Supported By</h1>
+                    <div className="box p-3 bg-white">
+                        <div className="row text-center mx-auto">
+                            <div className="logo-miica col-lg-4 col-md-4 mt-5 mt-lg-0">
+                                <img className="" src="images/logo/miica.png" />
+                            </div>
+                            <div className="logo-aktuaria col-lg-4 mt-5 mt-lg-0 text-center">
+                                <img className="" src="images/logo/LogoAKTUARIA.png" />
+                            </div>
+                            <div className="Logo-newera col-lg-4 col-md-4 mt-5 mt-lg-0">
+                                <img className="" src="images/logo/NWERA.png" />
+                            </div>
                         </div>
                     </div>
                 </div>
