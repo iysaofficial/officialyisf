2dbc80f9695536053acf1731016f077aff944480
young <youngscientist.iysa@gmail.com>
2024-01-29 16:22:19 +0700

    close regist

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -109,7 +109,7 @@ export default function Banner() {
             <div className="row align-items-center">
               <div className="col-md-6 order-2 order-md-1 text-center text-md-left">
                 <h1 className="text-white mb-4 banner-txt">
-                  Open Registration Youth International Science Fair 2024
+                Close Registration Youth International Science Fair 2024
                 </h1>
                 {/* <p className="text-white font-weight-light mb-5">(Open Registration)</p> */}
                 {/* <button className="ButtonItems">
