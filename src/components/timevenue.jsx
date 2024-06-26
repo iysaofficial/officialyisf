39682f05e1ba46f6776eb7abfc9464a98a350488
young <youngscientist.iysa@gmail.com>
2024-06-26 14:15:51 +0700

    timevenue

---
+++ src/components/timevenue.jsx
@@ -0,0 +1,56 @@
+import ButtonItems from "./ButtonItems"
+
+export default function Timevenue() {
+    return (
+        <>
+            <section className="feature section pt-0">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-6 ml-auto justify-content-center">
+
+                            {/* <!-- About Mockup --> */}
+
+                            {/* <div className="image-content" data-aos="fade-right"> */}
+                            <h1>Venue Malang Creative Center</h1>
+                                <img className="img-fluid" src="images/vektor/mcc.jpg" alt="iphone" />
+                            {/* </div> */}
+
+                        </div>
+                        <div className="col-lg-6 mr-auto align-self-center">
+                            <div className="feature-content">
+
+                            {
+                                /* <!-- About Title --> */
+                                <img className="img-fluid" src="images/vektor/time.jpg" alt="iphone" />
+
+                            }
+                            
+                                {/* <h1>Background</h1>
+                                <h2>Youth International Science Fair (YISF) </h2> */}
+
+                            {
+                                /* <!-- About Description --> */
+                            }
+
+                                {/* <p className="desc text-justify">The number of creativity and idea development
+                                    makes students produce many products that are
+                                    beneficial to society and the environment.
+                                    Creativity and the development of ideas as
+                                    outlined in the written work requires a place to
+                                    evaluate the results.</p>
+                                
+                                <ButtonItems 
+                                    text = "About More"
+                                    class = "btn-about-c btn-primary"
+                                    link = "/about"
+                                /> */}
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,8 @@ import VideoAE from "../components/VideoAE";
 import News from "../components/News";
 import Footer from "../components/Footer";
 
+import Timevenue from "../components/timevenue";
+
 function Home() {
     return (
         <>
@@ -14,12 +16,13 @@ function Home() {
             <C_About  
                 desc = "2020 is a year full of surprises. How come? Since the beginning of 2020, the world has been shocked by the virus that causes a pandemic (Covid 19) until 2021, this has not ended yet. Departing from this phenomenon, all life in the world changes drastical- ly, starting from the way we socialize, transportation models, learning systems in schools, and many others. Schools change online, employees work from home, basic public transportation and facilities, and other changes. The point is this pandemic has changed the entire structure of our lives."
             />
+            <Timevenue />
             <Mechanis />
             <Categories />
             <VideoAE />
             <News />
             <Supported />
-            <Footer />
+            {/* <Footer /> */}
         </>
     )
 }
