ec0e86b9e555a38554f538a579428f7faad632df
zaidanaap <zaidanaap@gmail.com>
2026-04-13 13:38:55 +0700

    UP AFTER EVENT 2026

--- src/components/VideoAE.jsx
+++ src/components/VideoAE.jsx
@@ -1,22 +1,24 @@
 export default function VideoAE() {
-    return (
-        <>
-            <section class="video-promo section bg-1">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="content-block">
-                                <h2>After Event YISF 2025</h2>
-                                <p>
-                                    </p>
-                                    <a data-fancybox href="https://youtu.be/dBIe6IuDkoI?si=uX2s9oIsP-bO1DAJ">
-                                        <i class="ti-control-play video"></i>
-                                    </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </>
-    )
+  return (
+    <>
+      <section class="video-promo section bg-1">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="content-block">
+                <h2>After Event YISF 2026</h2>
+                <p></p>
+                <a
+                  data-fancybox
+                  href="https://youtu.be/wo9SRnOKWxA?si=8TqlBu7UudsqfNzS"
+                >
+                  <i class="ti-control-play video"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
